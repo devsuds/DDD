de99e8418e97ed95be913989cc8f0236199f9d39
devsuds <sudipsahoo87@gmail.com>
2026-04-07 17:56:33 +0530

    chore: Hiding timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Tldraw } from 'tldraw';
 import { MainBoard } from './mainBoard';
-import { Timer } from './components/Timer';
+// import { Timer } from './components/Timer';
 import 'tldraw/tldraw.css';
 import './App.css';
 
 export default function App() {
   return (
     <div style={{ position: 'fixed', inset: 0, display: 'flex', flexDirection: 'column' }}>
-      <Timer />
+      {/* <Timer /> */}
       <div style={{ flex: 1, width: '100%', height: '100%' }}>
         <Tldraw autoFocus={false}>
           <MainBoard />
